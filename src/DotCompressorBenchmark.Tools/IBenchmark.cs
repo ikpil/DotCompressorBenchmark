946ee6742c6e41829c29ff626fa2e26647cf495a
ikpil <ikpil0@gmail.com>
2023-11-11 16:18:17 +0900

    first

---
+++ src/DotCompressorBenchmark.Tools/IBenchmark.cs
@@ -0,0 +1,7 @@
+﻿namespace DotCompressorBenchmark.Tools;
+
+public interface IBenchmark
+{
+    string Name => "unknown";
+    BenchmarkResult Start(string filename, byte[] srcBytes, byte[] dstBytes);
+}
